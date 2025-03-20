f0cb2de506d54e9b1c05df58275ee413d52ee596
xmagickit <silasforrest0322@gmail.com>
2025-03-20 21:56:14 +0000

    Add jazzmin interface to django admin panel :lipstick:

--- team_4/settings.py
+++ team_4/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(","
 
 INSTALLED_APPS = [
     # predefined django apps
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
