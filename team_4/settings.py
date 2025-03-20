a770b245e3c23aec90c60222e7f09f4b17e37b63
xmagickit <silasforrest0322@gmail.com>
2025-03-20 18:10:10 +0000

    Responsive header and Women in history section

--- team_4/settings.py
+++ team_4/settings.py
@@ -103,6 +103,10 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default auto field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
