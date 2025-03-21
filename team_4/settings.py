3b26c2b123b3c29153a6dd7ffaf58c3b25d66549
xmagickit <silasforrest0322@gmail.com>
2025-03-21 16:20:45 +0000

    Prepare code for heroku deployment

--- team_4/settings.py
+++ team_4/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1", ".herokuapp.com").split(",")
 
 
 # Application definition
