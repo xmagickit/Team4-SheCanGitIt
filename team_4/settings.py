1cc17dd321802bb209bfdf481744fcb610658a15
xmagickit <silasforrest0322@gmail.com>
2025-03-21 18:45:23 +0000

    added herstory js for screen loader

--- team_4/settings.py
+++ team_4/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "True") == "False"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1", ".herokuapp.com").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,.herokuapp.com").split(",")
 
 
 # Application definition
