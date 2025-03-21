8e408c0e9f24f2ea9a6ef47eeaf29f47e0bd82c7
xmagickit <silasforrest0322@gmail.com>
2025-03-21 22:26:19 +0000

    Updated RetroEditorConfig with proper name and verbose name

--- retro_editor/apps.py
+++ retro_editor/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RetroEditorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'retro_editor'
+    verbose_name = 'Retro Code Editor'
