11c8fa656c557cb0b1895e8a5745cb9e475b605b
xmagickit <silasforrest0322@gmail.com>
2025-03-21 09:35:33 +0100

    Start work on chatroom functionality

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.friendly_name})"
