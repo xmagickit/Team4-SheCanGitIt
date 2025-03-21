5a4798305c5cf54484f65d3c0fffaf4fd1406443
xmagickit <silasforrest0322@gmail.com>
2025-03-21 18:17:12 +0100

    add variable logic for deployment to Heroku

--- team_4/settings.py
+++ team_4/settings.py
@@ -118,9 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'CLOUDINARY_URL': os.getenv('CLOUDINARY_URL'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
