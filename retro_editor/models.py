667ee5401478e5685c785e5101a8e38cd3cf8ac0
xmagickit <silasforrest0322@gmail.com>
2025-03-21 21:36:27 +0000

    Defined language choices for supported coding languages

--- retro_editor/models.py
+++ retro_editor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+LANGUAGE_CHOICES = [
+    ('html', 'HTML'),
+    ('css', 'CSS'),
+    ('js', 'JavaScript'),
+    ('py', 'Python'),
+]
 # Create your models here.
