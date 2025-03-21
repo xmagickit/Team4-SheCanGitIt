23a0abfe94e97e19316f0cfa776a4e357da78f65
xmagickit <silasforrest0322@gmail.com>
2025-03-21 18:59:12 +0000

    updated herstory and corrected debug

--- team_4/settings.py
+++ team_4/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security settings
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "True") == "False"
+DEBUG = os.environ.get("DEBUG", "True") != "False"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,.herokuapp.com").split(",")
 
 
