7f7da32f360049adf96b93e4a2210446e8b42c2b
xmagickit <silasforrest0322@gmail.com>
2025-03-21 21:39:08 +0000

    Added TechPioneerTip model for inspirational tips from women in tech :sparkle:

--- retro_editor/models.py
+++ retro_editor/models.py
@@ -19,3 +19,11 @@ class CodeSnippet(models.Model):
     
     def __str__(self):
         return f"{self.title} ({self.language}) by {self.user.username}"
+
+class TechPioneerTip(models.Model):
+    pioneer = models.CharField(max_length=100)
+    tip_content = models.TextField()
+    language = models.CharField(max_length=20, choices=LANGUAGE_CHOICES, blank=True)
+    
+    def __str__(self):
+        return f"Tip from {self.pioneer}"
