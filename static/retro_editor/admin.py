aad33fd8aab13ffc2445b38e439018e67034ed41
xmagickit <silasforrest0322@gmail.com>
2025-03-21 22:19:44 +0000

    Added admin configurations for TechPioneerTip model

--- static/retro_editor/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import CodeSnippet, TechPioneerTip
-
-@admin.register(CodeSnippet)
-class CodeSnippetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'language', 'user', 'created_at', 'is_public')
-    list_filter = ('language', 'is_public', 'created_at')
-    search_fields = ('title', 'code_content', 'user__username')
-    date_hierarchy = 'created_at'
