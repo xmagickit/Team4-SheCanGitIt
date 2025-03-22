78aed011a9001dee61254e15a741c4c6cc734436
xmagickit <silasforrest0322@gmail.com>
2025-03-22 14:51:43 +0000

    Add contrib sites and site 1 id and started to customise allauth

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def profile_view(request, username=None):
+    return render(request, 'user_profile/profile.html', {
+        'username': username or request.user.username  # Optional: pass username
+    })
+    
+def edit_profile(request):
+    return render(request, 'user_profile/edit_profile.html')
+
+def profile_list(request):
+    return render(request, 'user_profile/profile_list.html')
+
