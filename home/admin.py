2b013b3d8892a3cdadb31105b9e036f7ab5ff859
xmagickit <silasforrest0322@gmail.com>
2025-03-22 20:27:30 +0000

    Resolved the image loading for women in tech and add slideshow

--- home/admin.py
+++ home/admin.py
@@ -7,7 +7,10 @@ class WomenInTechAdmin(admin.ModelAdmin):
     list_display = ("name", "preview_image")
 
     def preview_image(self, obj):
-        if obj.image:
-            return format_html('<img src="{}" width="50" style="border-radius:4px;" />', obj.image.url)
+        if obj.image_url:
+            return format_html(
+                '<img src="{}" width="50" style="border-radius:4px;" />', obj.image_url
+            )
         return "-"
+    
     preview_image.short_description = "Image"
--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class WomenInTech(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField()
-    image = models.ImageField(upload_to='women/')
+    image_url = models.URLField("Image URL", max_length=500, blank=True)
 
     def __str__(self):
         return self.name
