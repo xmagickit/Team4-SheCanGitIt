8a6e1220337f25a5c23079df421379d234a9244f
xmagickit <silasforrest0322@gmail.com>
2025-03-23 19:19:29 +0000

    user can update profile with more details. Choice of avatars.

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'experience_level', 'is_mentor', 'seeking_buddy')
+    list_filter = ('experience_level', 'is_mentor', 'seeking_buddy')
+    search_fields = ('user__username', 'user__email', 'skills')
