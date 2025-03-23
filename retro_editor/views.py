f6cc5d031edd84210bebc6c3c28a04c84d145afb
xmagickit <silasforrest0322@gmail.com>
2025-03-23 22:27:38 +0000

    Fix missing import in views.py :bug:

--- retro_editor/views.py
+++ retro_editor/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from .models import CodeSnippet, TechPioneerTip
+from .models import CodeSnippet, TechPioneerTip, CodeSample
 from .forms import CodeSnippetForm
 import random
 
